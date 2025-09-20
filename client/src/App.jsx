bbad2980f6d4cbb0ae55abfe3f838cf786a307cb
pedrodimarco <pdimarco97@gmail.com>
2025-09-20 14:12:38 -0300

    create registration page and logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+
 function App() {
   return (
-    <div>
-      <h1 class="text-4xl font-bold">Hola mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<h1>Inicio</h1>}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/tasks" element={<h1>Task page</h1>}></Route>
+        <Route path="/add-task" element={<h1>New Task</h1>}></Route>
+        <Route path="/tasks/:id" element={<h1>Update task</h1>}></Route>
+        <Route path="/profile" element={<h1>Profile</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js"; //authentication routes
 import tasksRoutes from "./routes/tasks.routes.js"; //tasks routes
@@ -8,6 +9,11 @@ import tasksRoutes from "./routes/tasks.routes.js"; //tasks routes
 const app = express();
 
 //Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
